ae0849f933b89880f71ec22b4a600f4afa38244c
Anuka <anukazan@gmail.com>
2022-06-19 05:07:30 -0700

    prompt user

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const Employee = require('./lib/Employee');
+const { writeFile, copyFile } = require('./src/generate-site.js');
+
+const generatePage = require('./src/page-template.js');
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const promptUser = () => {
-    return inquirer.prompt([
+function Game(){
+    this.teamArray = [];
+}
+
+Game.prototype.initializeGame = function(){
+    inquirer.prompt([
         {
             type:'input',
             name: 'name',
@@ -27,19 +34,94 @@ const promptUser = () => {
             type:'input',
             name: 'officeNumber',
             message: "What is the team manager's office number?"
-        },
-        {
-            type: 'list',
-            name: 'addEmployee',
-            message: 'Would you like to add another employee or finish building your team?',
-            choices: ['Engineer', 'Intern', 'Finish']
         }
-    ]);
+    ])
+    .then(({ name, id, email, officeNumber}) => {
+        this.manager = new Manager(name, id, email, officeNumber);
+        this.teamArray.push(this.manager);
+        this.ifEmployee();
+    });
+}
+
+Game.prototype.ifEmployee = function(){
+    inquirer.prompt({
+        type: 'list',
+        name: 'addEmployee',
+        message: 'Would you like to add another employee or finish building your team?',
+        choices: ['Engineer', 'Intern', 'Finish']
+    })
+    .then(({addEmployee}) => {
+        if (addEmployee === 'Engineer'){
+           inquirer.prompt([
+            {
+                type:'input',
+                name: 'name',
+                message: "What is the engineer's name?"
+            },
+            {
+                type:'input',
+                name: 'id',
+                message: "What is the engineer's employee ID?"
+            },
+            {
+                type:'input',
+                name: 'email',
+                message: "What is the engineer's email address?"
+            },
+            {
+                type:'input',
+                name: 'github',
+                message: "What is the engineer's github username?"
+            }
+        ]).then(({ name, id, email, github }) => {
+                this.engineer = new Engineer(name, id, email, github);
+                this.teamArray.push(this.engineer);
+            });
+
+        } else if (addEmployee === 'Intern'){
+            inquirer.prompt([
+                {
+                    type:'input',
+                    name: 'name',
+                    message: "What is the intern's name?"
+                },
+                {
+                    type:'input',
+                    name: 'id',
+                    message: "What is the intern's employee ID?"
+                },
+                {
+                    type:'input',
+                    name: 'email',
+                    message: "What is the intern's email address?"
+                },
+                {
+                    type:'input',
+                    name: 'school',
+                    message: "What is the intern's school?"
+                }
+            ])
+            .then(({ name, id, email, school }) => {
+                this.intern = new Intern(name, id, email, school);
+                this.teamArray.push(this.intern);
+                this.ifEmployee();
+            });
+            
+        } else{
+            console.log("You are finished building your team");
+            console.log(this.teamArray);
+            return;
+        }
+    })
 };
 
-promptUser();
-module.exports = Prompts;
+new Game().initializeGame();
 
-// .then(({ name })) => {
-//     this.manager = new Manager(name)
-// }
+// .then( generatePage(this.teamArray))
+//     .then(pageHTML => {
+//         writeFile(pageHTML);
+//     })
+//     .then(copyFile())
+//     .catch(err => {
+//         console.log(err);
+//     })
