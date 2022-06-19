0494f01ddf3794735da6f7b1c49a37c613ba8e8b
Anuka <anukazan@gmail.com>
2022-06-19 07:47:27 -0700

    generate HTML for each feature

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const { writeFile, copyFile } = require('./src/generate-site.js');
-
 const generatePage = require('./src/page-template.js');
 
+//team members
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-function Game(){
-    this.teamArray = [];
-}
+teamArray = [];
 
-Game.prototype.initializeGame = function(){
-    inquirer.prompt([
+//PROMPT USER ABOUT TEAM MANAGER 
+const initializePrompt = () => {
+    return inquirer.prompt([
         {
             type:'input',
             name: 'name',
@@ -37,91 +35,99 @@ Game.prototype.initializeGame = function(){
         }
     ])
     .then(({ name, id, email, officeNumber}) => {
-        this.manager = new Manager(name, id, email, officeNumber);
-        this.teamArray.push(this.manager);
-        this.ifEmployee();
+        const manager = new Manager(name, id, email, officeNumber);
+        teamArray.push(manager);
+
+        checkIfAdd(); //CHECK IF USER WANTS TO ADD ANOTHER EMPLOYEE
     });
 }
 
-Game.prototype.ifEmployee = function(){
+
+
+//ADD NEW EMPLOYEE
+const addEmployee = ()=> {
+    console.log(`
+    =================
+    ADDING NEW EMPLOYEE
+    =================
+    `);
+
+    inquirer.prompt([    
+        {
+            type:'input',
+            name: 'name',
+            message: "What is the employee's name?"
+        },
+        {
+            type:'input',
+            name: 'id',
+            message: "What is the employee's ID?"
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: "What is the employee's email address?"
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: ['Engineer', 'Intern']
+        }
+    ]).then(({ name, id, email, role }) => { //DIFFERENT QUESTIONS IF THEY ARE ENGINEER OR INTERN
+        let addedQuestion = "";
+        if (role === 'Engineer'){
+            addedQuestion = "Github Username";
+        } else {
+            addedQuestion = "school name";
+        }
+
+        inquirer.prompt(
+            {
+                type:'input',
+                name: 'addedQuestion',
+                message: `What is the employee's ${addedQuestion}?`
+            }
+        ).then(({ addedQuestion}) => {
+            let employee;
+            if (role === 'Engineer'){
+                employee = new Engineer(name, id, email, addedQuestion);
+            } else {
+                employee = new Intern(name, id, email, addedQuestion);
+            }
+
+            teamArray.push(employee);
+
+            checkIfAdd(); //CHECK IF USER WANTS TO ADD ANOTHER USER
+
+        })
+    })
+
+}   
+
+//CHECK IF USER WANTS TO ADD ANOTHER EMPLOYEE
+const checkIfAdd = () => {
     inquirer.prompt({
         type: 'list',
-        name: 'addEmployee',
-        message: 'Would you like to add another employee or finish building your team?',
-        choices: ['Engineer', 'Intern', 'Finish']
-    })
-    .then(({addEmployee}) => {
-        if (addEmployee === 'Engineer'){
-           inquirer.prompt([
-            {
-                type:'input',
-                name: 'name',
-                message: "What is the engineer's name?"
-            },
-            {
-                type:'input',
-                name: 'id',
-                message: "What is the engineer's employee ID?"
-            },
-            {
-                type:'input',
-                name: 'email',
-                message: "What is the engineer's email address?"
-            },
-            {
-                type:'input',
-                name: 'github',
-                message: "What is the engineer's github username?"
-            }
-        ]).then(({ name, id, email, github }) => {
-                this.engineer = new Engineer(name, id, email, github);
-                this.teamArray.push(this.engineer);
-            });
+        name: 'anotherEmployee',
+        message: 'Would you like to add another employee?',
+        choices: ['Yes', 'No']
+    }).then(({ anotherEmployee }) => {
 
-        } else if (addEmployee === 'Intern'){
-            inquirer.prompt([
-                {
-                    type:'input',
-                    name: 'name',
-                    message: "What is the intern's name?"
-                },
-                {
-                    type:'input',
-                    name: 'id',
-                    message: "What is the intern's employee ID?"
-                },
-                {
-                    type:'input',
-                    name: 'email',
-                    message: "What is the intern's email address?"
-                },
-                {
-                    type:'input',
-                    name: 'school',
-                    message: "What is the intern's school?"
-                }
-            ])
-            .then(({ name, id, email, school }) => {
-                this.intern = new Intern(name, id, email, school);
-                this.teamArray.push(this.intern);
-                this.ifEmployee();
-            });
-            
+        //IF YES, ADD ANOTHER EMPLOYEE
+        if (anotherEmployee ==='Yes'){
+            addEmployee();
         } else{
-            console.log("You are finished building your team");
-            console.log(this.teamArray);
-            return;
+
+            //OTHERWISE READY TO COMPLETE TEAM ARRAY
+            console.log(teamArray);
+            generatePage(teamArray);
         }
     })
-};
+}
 
-new Game().initializeGame();
 
-// .then( generatePage(this.teamArray))
-//     .then(pageHTML => {
-//         writeFile(pageHTML);
-//     })
-//     .then(copyFile())
-//     .catch(err => {
-//         console.log(err);
-//     })
+initializePrompt()
+
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,72 +1,148 @@
-const generateTeam = teamArray => {
+const fs = require('fs');
 
+const generateManager = ManagerData => {
     return `
-        <section class = "my-3" id = "portfolio">
-            <h2 class = "text-dark bg-primary p-2 display-inline-block"> Work </h2>
-            <div class="flex-row justify-space-between">
-            ${teamArray   
-                .map(({ name, id, email, ...details }) => {
-                    return `
-                    <div class="col-12 mb-2 bg-dark text-light p-3">
-                        <h3 class="portfolio-item-title text-light"> ${name} </h3>
-                        <h5 class="portfolio-languages">
-                            Id:${id}
-                            Email: ${email}
-                        </h5>
+        <div class = "column">
+            <div class = "card">
+                <div class="card-content">
+                    
+                    <div class="media-content">
+                        <p class="title is-4"> ${ManagerData.name}</p>
+                        <p class="subtitle is-6"> Intern </p>
+                    </div>
+                
+                    <div class="content">
+                        <p>ID: ${ManagerData.id}</p>
+                        <p>Email:<a href="mailto:${ManagerData.email}">${ManagerData.email}</a></p>
+                        <p>Office Number: ${ManagerData.officeNumber}</p>
+                    </div>
 
-                        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i> View Project on Github </a>
-                    </div>  
-                    `;
-                })
-            .join('')}
-        </section>
-    `;
-};
+                </div>
+            </div>
+        </div>
+    `
+}
 
 
-module.exports = templateData => {
+const generateEngineer= EngineerData => {
+    return `
+        <div class = "column">
+            <div class = "card">
+                <div class="card-content">
+                    
+                    <div class="media-content">
+                        <p class="title is-4"> ${EngineerData.name}</p>
+                        <p class="subtitle is-6"> Engineer </p>
+                    </div>
+                
+                    <div class="content">
+                        <p>ID: ${EngineerData.id}</p>
+                        <p>Email:<a href="mailto:${EngineerData.email}">${EngineerData.email}</a></p>
+                        <a href="https://github.com/${EngineerData.github}">GitHub</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `
+}
+
+
+const generateIntern = internData => {
+    return `
+        <div class = "column">
+            <div class = "card">
+                <div class="card-content">
+                    
+                    <div class="media-content">
+                        <p class="title is-4"> ${internData.name}</p>
+                        <p class="subtitle is-6"> Intern </p>
+                    </div>
+                
+                    <div class="content">
+                        <p>ID: ${internData.id}</p>
+                        <p>Email:<a href="mailto:${internData.email}">${internData.email}</a></p>
+                        <p>School: ${internData.school}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `
+}
+
+
+
+const generateCard = cardData =>{
+    cardArray = []; 
+        //GENERATE EACH CARD BASED ON ROLE 
+     for (let i = 0; i < cardData.length; i++) {
+         const role = cardData[i].getRole(); 
+         
+
+         if (role === 'Manager') {
+            const manager = generateManager(cardData[i]);
+            cardArray.push(manager);
+            //console.log(manager);
+         } 
+         
+         if (role === 'Engineer') {
+            const engineer = generateEngineer(cardData[i]);
+            cardArray.push(engineer);
+            //console.log(engineer);
+         }
+         
+         if (role === 'Intern'){
+            const intern = generateIntern(cardData[i]);
+            cardArray.push(intern);
+            //console.log(intern);
+         }
+     }
+ 
+     const newCard = cardArray.join('')
+
+     return generatePage(newCard); 
+}
+
+const generatePage = templateData => {
     console.log("SITE GENERATED");
 
-    // const {projects, about, ...header} = templateData; //grabbing any value by calling out the key value of the property
-    // // ^ any unsaved value is saved in header
+    const codeHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+        <title> Team Profile Generator </title>
+    </head>
 
-    // return `
-    // <!DOCTYPE html>
-    // <html lang="en">
+    <body>
+        <header class="hero is-large">
+            <div class="hero-head is-center">
+                <div class="container is-center">
+                    <h1 class="title is-center"> Team Profile Generator </h1>
+                </div>
+            </div>
+        </header>
 
-    // <head>
-    //     <meta charset = "UTF-8">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //     <title>Portfolio Demo</title>
-    //     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    //     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    //     <link rel="stylesheet" href="style.css">
-    // </head>
+        <main class = "container">
+            <div class = "columns">
+                ${templateData};
+            </div>
+        </main>
+    `;
+
+    //make HTML file with code
+    fs.writeFile("./dist/index.html", codeHTML, function(err){
+        if (err){
+            console.log
+        }
+    });
+}
+
+
     
-    // <body>
-    //     <header>
-    //         <div class="container flex-row justify-space-between align-center py-3">
-    //             <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-    //             <nav class="flex-row">
-    //             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-    //                 header.github
-    //             }">GitHub</a>
-    //             </nav>
-    //         </div>
-    //     </header>
 
-    //     <main class="container my-5">
-    //             ${generateAbout(about)}
-
-    //             ${generateProjects(projects)}
-    //     </main>
-
-    //     <footer class="container text-center py-3">
-    //         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
-    //     </footer>   
-    // </body>
-    // </html>    
-    // `;
-};
-
+module.exports = generateCard;
